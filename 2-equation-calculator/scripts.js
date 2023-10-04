e6bcd3edaa72ed70fdd3fd72e479ee4540b4ef27
Charlie Phillips <cphillips24@hanoberstudents.org>
2023-10-04 07:59:47 -0400

    10/4/2023

---
+++ 2-equation-calculator/scripts.js
@@ -0,0 +1,33 @@
+function calculateAcceleration(){
+    var intialvelocity = parseInt(document.forms[0].elements['velocityi'].value);
+    var finalvelocity = document.forms[0].elements['velocityf'].value;
+    var time = document.forms[0].elements['time'].value;
+    var acceleration = (finalvelocity - intialvelocity)/time ;
+    if(isNaN(acceleration)){
+        document.getElementById('acceleration').innerHTML = 'Need More Values';
+
+    }else{
+    document.getElementById('acceleration').innerHTML = "The Acceleration is " + acceleration + " (m/s²)";
+    }
+}
+
+function calculateForce(){
+    var mass = document.forms[1].elements['mass'].value;
+    var acceleration = document.forms[1].elements['acceleration'].value;
+    var force = mass * acceleration ;
+    if(isNaN(force)){
+        document.getElementById('force').innerHTML = 'Need More Values';
+
+    }else{
+    document.getElementById('force').innerHTML = "The Force is " + force + " (N)";
+    }
+}
+
+function calculateKineticEnergy(){
+    var mass = document.forms[2].elements['mass'].value;
+    var velocity = document.forms[2].elements['velocity'].value;
+    var kineticEnergy = .5 * mass * velocity * velocity ;
+    document.getElementById('kineticenergy').innerHTML = "The Kinetic Energy is " + kineticEnergy + " (J)";
+    
+}
+
